25cbbc4ee784ea5ee2d1d5de1adc9c6a40a71d1c
Joel Ohman <Joel Ohman>
2016-04-06 17:49:37 -0600

    complete gross pay

---
+++ grosspay.py
@@ -0,0 +1,8 @@
+
+hours=input ("How Many Hours?")
+wage=input ("What is Your Hourly Wage?")
+hours=float (hours)
+wage=float (wage)
+gross= hours*wage
+gross= str(gross)
+print ("Gross Wage: " + gross)
